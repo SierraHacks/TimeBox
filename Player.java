03facc37cb4006c6245cc2635c1a52961e37b3cf
Pranav P <153451679+ptowncs@users.noreply.github.com>
2026-08-19 22:07:18 +0000

    implement hashmaps into inventory and crafting recipes

--- Player.java
+++ Player.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Map;
 public class Player{
     static int level = 0;
     private int health;
-    private ArrayList<String> inventory = new ArrayList<>();
+    private Weapon weapon;
+    private Map<String, Integer> inventory = new Hashmap<>();
     private double xcoord;
     private double ycoord;
 
--- Weapon.java
+++ Weapon.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
+import java.util.Map;
 public class Weapon {
     private String name;
-    private ArrayList<String> recipe = new ArrayList<>();
+    private Map<String, Integer> recipe = new Hashmap<>();
     private int damage;
-    public Weapon(ArrayList<String> recipe, int damage, String name){
+    public Weapon(HashMap<String, Integer> recipe, int damage, String name){
         this.name = name;
         this.recipe = recipe;
         this.damage = damage;
