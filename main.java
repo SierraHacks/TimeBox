19b0280852071ea28b60991791bb883ca77f0883
Ishan Paul <2020.ishan.paul@gmail.com>
2026-08-24 10:31:21 -0700

    added Java Swing

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 import java.awt.*;
+import javax.swing.*;
 
 public class main {
     public static void main(String[] args) { 
-        
+        JFrame frame = new JFrame("Time Box");
+
+        // Add panel in center of frame
+        JPanel center = new JPanel(new BorderLayout());
+        center.setBackground(Color.cyan);
+
     }
   
   }  
