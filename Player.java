b37e44d72dc4e54d0cae25da23e1757114e69c86
Pranav P <153451679+ptowncs@users.noreply.github.com>
2026-08-26 17:19:06 +0000

    Add crafting logic

--- Player.java
+++ Player.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
 import java.util.HashMap;
 import java.util.List;
-public class Player{
+
+public class Player {
     static int level = 0;
     private int health;
     private Weapon weapon;
@@ -11,25 +13,47 @@ public class Player{
     private double xcoord;
     private double ycoord;
 
-//figure out weapons/crafting
-    public Player(int health, int xcoord, int ycoord){
+    // figure out weapons/crafting
+    public Player(int health, int xcoord, int ycoord) {
         this.health = health;
         this.xcoord = xcoord;
         this.ycoord = ycoord;
     }
-    public int attack(){
-        return 5+this.weapon.getDamage();
+
+    public int attack() {
+        return 5 + this.weapon.getDamage();
     }
-    public void takeDamage(int damage, boolean block){
-        if(!block){
+
+    public void takeDamage(int damage, boolean block) {
+        if (!block) {
             this.health -= damage;
         }
     }
-    public void craft(){
-        //figure out crafting logic based on inventory
+
+    private static Weapon weaponCheck(String weaponChoice) {
+        for (int i = 0; i < Weapon.weaponList.size(); i++) {
+            Weapon w = Weapon.weaponList.get(i);
+            if (weaponChoice.equals(w.getName())) {
+                return w;
+            }
+        }
+        return null;
     }
-    public void equipWeapon(Weapon weapon){
-        if(this.weapons.contains(weapon)){
+
+    public void craft(Weapon w) {
+        if (Player.weaponCheck(w.getName()) != null) {
+            if (this.inventory.keySet().containsAll(w.getRecipe().keySet())) {
+                this.weapons.add(w);
+                for (String key : w.getRecipe().keySet()) {
+                    this.inventory.put(key, this.inventory.get(key) - w.getRecipe().get(key));
+                }
+                this.weapons.add(w);
+            }
+        }
+    }
+
+    public void equipWeapon(Weapon weapon) {
+        if (this.weapons.contains(weapon)) {
             this.weapon = weapon;
         }
     }
--- Weapon.java
+++ Weapon.java
@@ -1,6 +1,9 @@
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 public class Weapon {
+    static ArrayList<Weapon> weaponList = new ArrayList<>();
     private String name;
     private Map<String, Integer> recipe = new HashMap<>();
     private int damage;
@@ -8,9 +11,16 @@ public class Weapon {
         this.name = name;
         this.recipe = recipe;
         this.damage = damage;
+        weaponList.add(this);
     }
     public int getDamage(){
         return this.damage;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public Map<String, Integer> getRecipe() {
+        return Collections.unmodifiableMap(this.recipe);
     }
 
     
