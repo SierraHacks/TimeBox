5dea0701de5e1ac1837c20aa7635823f91e1e98f
SierraHacks <ss1967@pleasantonusd.net>
2026-08-26 13:31:19 -0700

    added application to main.java

--- main.java
+++ main.java
@@ -1,13 +1,37 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class main {
-    public static void main(String[] args) { 
+    public static void createAndShowGUI(){
         JFrame frame = new JFrame("Time Box");
+
+        // Add title
+        JLabel title = new JLabel("Time Box", SwingConstants.CENTER);
+        Font heading = new Font("Times Roman", Font.BOLD, 20);
+        title.setFont(heading);
+        title.setPreferredSize(new Dimension(300,50));
+        title.setOpaque(true);
+        title.setBackground(Color.decode("#f0e9e9"));
+        frame.getContentPane().add(title, BorderLayout.PAGE_START);
 
         // Add panel in center of frame
         JPanel center = new JPanel(new BorderLayout());
-        center.setBackground(Color.cyan);
+        center.setPreferredSize(new Dimension(1000,1000));
+        center.setBackground(Color.white);
+        frame.getContentPane().add(center, BorderLayout.CENTER);
+
+        //Show the frame
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) { 
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                createAndShowGUI();
+            }
+        });
 
     }
   
