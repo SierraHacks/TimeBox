233313decf8ac27b61aedb54bbff65814ea826c7
AttreyKoul <139191641+AttreyKoul@users.noreply.github.com>
2026-08-28 23:15:39 +0000

    Started making the load_sprites() function

--- Dragon.java
+++ Dragon.java
@@ -4,8 +4,8 @@ import javax.swing.Timer;
 
 public class Dragon extends NonPlayer {
     public enum State { IDLE, DASH, CLAW_SWIPE, FIRE_BREATH, DEFEAT }
-    private State cS = State.IDLE; //short for currentState, state determines current movement type
-    
+    private State currentState = State.IDLE; //short for currentState, state determines current movement type
+
     
     private int x, y;
     private int currentFrame = 0;
@@ -16,8 +16,16 @@ public class Dragon extends NonPlayer {
     private BufferedImage[] dashFrames;
     private BufferedImage[] fireFrames;
 
-    //todo
-    //Add code to get sprites from dragon.png
-    //Add in constructor
+    public Dragon(String name, int health, int damage, int startX, int startY) {
+        super(name, health, damage);
+        this.x = startX;
+        this.y = startY;
+        loadSprites(); 
+        currentAnim = idleFrames;
+    }
+
+    private void loadSprites() {
+        
+    }
 
 }
