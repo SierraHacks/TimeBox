5646fbf39d96a6be40bc09d890d781f3410819a5
AttreyKoul <139191641+AttreyKoul@users.noreply.github.com>
2026-08-28 23:49:44 +0000

    Adding msising libraries

--- Dragon.java
+++ Dragon.java
@@ -1,7 +1,9 @@
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.swing.Timer;
-
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 public class Dragon extends NonPlayer {
     public enum State { IDLE, DASH, CLAW_SWIPE, FIRE_BREATH, DEFEAT }
     private State currentState = State.IDLE; //short for currentState, state determines current movement type
