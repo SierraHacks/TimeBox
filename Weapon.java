acb2e742a8007a49bd05df2bd93a5aa9d96f37aa
Pranav P <153451679+ptowncs@users.noreply.github.com>
2026-08-28 16:36:17 +0000

    add weapon initializations

--- Weapon.java
+++ Weapon.java
@@ -1,9 +1,10 @@
 import java.util.Map;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 public class Weapon {
-    static ArrayList<Weapon> weaponList = new ArrayList<>();
+    static ArrayList<Weapon> weaponList = new ArrayList<>(List.of());
     private String name;
     private Map<String, Integer> recipe = new HashMap<>();
     private int damage;
@@ -21,6 +22,33 @@ public class Weapon {
     public String getName(){
         return this.name;
     }
+public static void initWeapons() {
+    Weapon sword = new Weapon(
+        new HashMap<>(Map.of("Wood", 2, "Steel", 5)),
+        10,
+        "Sword",
+        5
+    );
+
+    Weapon warHammer = new Weapon(
+        new HashMap<>(Map.of("Wood", 2, "Steel", 10)),
+        5,
+        "War Hammer",
+        3
+    );
+
+    Weapon poleaxe = new Weapon(
+        new HashMap<>(Map.of("Wood", 5, "Steel", 15)),
+        25,
+        "Poleaxe",
+        10
+    );
+    Weapon.weaponList.add(sword);
+    Weapon.weaponList.add(warHammer);
+    Weapon.weaponList.add(poleaxe);
+
+}
+
     public Map<String, Integer> getRecipe() {
         return Collections.unmodifiableMap(this.recipe);
     }
