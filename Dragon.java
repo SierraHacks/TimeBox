ca58f458a8c6dc7a61be567bf57cb3297a8ae3a1
AttreyKoul <139191641+AttreyKoul@users.noreply.github.com>
2026-08-28 23:05:58 +0000

    added in fields

--- Dragon.java
+++ Dragon.java
@@ -1,8 +1,23 @@
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import java.awt.event.*;
-import java.util.Map;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.Timer;
+
+public class Dragon extends NonPlayer {
+    public enum State { IDLE, DASH, CLAW_SWIPE, FIRE_BREATH, DEFEAT }
+    private State cS = State.IDLE; //short for currentState, state determines current movement type
+    
+    
+    private int x, y;
+    private int currentFrame = 0;
+    private BufferedImage[] currentAnim;
+    
+    //Arrays for the animations
+    private BufferedImage[] idleFrames;
+    private BufferedImage[] dashFrames;
+    private BufferedImage[] fireFrames;
+
+    //todo
+    //Add code to get sprites from dragon.png
+    //Add in constructor
+
+}
